10700407d5ead20a16d554039fce04b61dc62f7c
Kevin Isom <kevin.isom@gmail.com>
2018-10-20 22:23:01 +1300

    Add react, parcel, and eslint react plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-module.exports = {}
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'mini.css'
+console.log('app')
+ReactDOM.render(<h1>Daily React App</h1>, document.getElementById('root'))
