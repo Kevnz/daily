15b04d1047076e05f2d213bbbac791e8024b83a6
Kevin Isom <kevin@isom.nz>
2019-04-29 12:24:02 +1200

    Show using bent to download a file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-module.exports = {}
+const fs = require('fs').promises
+const bent = require('bent')
+const getBuffer = bent('buffer')
+
+;(async () => {
+  let buffer = await getBuffer('https://javascript.rodeo/js-rodeo.png')
+  await fs.writeFile('js-rodeo.png', buffer)
+})()
