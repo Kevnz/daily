2bd96a23a790870baab0c45d7085f9d3372922e8
Kevin Isom <kevin.isom@gmail.com>
2019-05-03 17:07:49 +1200

    show it off

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-module.exports = {}
+require('./log')
+const obj = {
+  name: 'Obj',
+  values: [1,2,3]
+}
+console.log(`\n`)
+console.log('LOG')
+console.info('INFO')
+console.warn('WARN')
+console.error('ERROR')
+console.log(`\n`)
+console.log('LOG', obj)
+console.info('INFO', obj)
+console.warn('WARN', obj)
+console.error('ERROR', obj)
+console.log(`\n`)
